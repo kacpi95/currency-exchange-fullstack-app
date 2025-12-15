3444468f432b62edf4bf17f769775f3b01b2f244
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-15 16:22:17 +0100

    add: AuthNavigator component with navigation login and register

---
+++ frontend/navigation/AuthNavigator.jsx
@@ -0,0 +1,12 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function AuthNavigator() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='Register' component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+}
