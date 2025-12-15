20339d67b986d6c8398787f1f8b752d178858656
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-15 16:34:43 +0100

    feat: add RootNavigator with AuthContext

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,7 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { useContext } from 'react';
+import { AuthContext, AuthProvider } from './context/AuthContext';
+import AppNavigator from './navigation/AppNavigator';
+import AuthNavigator from './navigation/AuthNavigator';
+
+const RootNavigator = () => {
+  const { token } = useContext(AuthContext);
+  return token ? <AppNavigator /> : <AuthNavigator />;
+};
 
 export default function App() {
-  return;
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </AuthProvider>
+  );
 }
-
-const styles = StyleSheet.create({});
