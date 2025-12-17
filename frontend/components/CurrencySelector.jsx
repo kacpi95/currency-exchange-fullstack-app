8832d744bc8c0468a34c0645f1c9b691b8010a28
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-17 17:27:37 +0100

    style: add style to CurrencySelector

--- frontend/components/CurrencySelector.jsx
+++ frontend/components/CurrencySelector.jsx
@@ -31,4 +31,41 @@ export default function CurrencySelector({ label, value, onSelect }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  currencyRow: {
+    flexDirection: 'row',
+    gap: 12,
+    marginBottom: 10,
+  },
+  subtitle: {
+    marginBottom: 6,
+    marginTop: 12,
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#028090',
+  },
+
+  currencyButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#a3d2ca',
+    backgroundColor: '#fff',
+  },
+
+  currencyActive: {
+    backgroundColor: '#028090',
+    borderColor: '#028090',
+  },
+
+  currencyText: {
+    fontWeight: '600',
+    color: '#05668d',
+  },
+
+  currencyTextActive: {
+    color: '#fff',
+  },
+});
