50c06db231a48497a32b671fc236205a9aa8c3e1
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-18 20:07:59 +0100

    chore: create centralized color variables

---
+++ frontend/styles/colors.js
@@ -0,0 +1,17 @@
+export default {
+  //Background
+  backgroundDefault: '#e8f7f2',
+  backgroundWhite: '#ffffff',
+  logoutCardBackground: '#fdf1f1',
+
+  //Text
+  textPrimary: '#05668d',
+  textSecondary: '#028090',
+  textTertiary: '#999',
+  textError: '#c1121f',
+  textDisabled: '#666',
+
+  //Border
+  borderDefault: '#a3d2ca',
+  logoutCardBorder: '#f5c2c2',
+};
